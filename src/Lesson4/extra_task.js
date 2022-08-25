abf092026bed3d2155d15443e32c3c2ee6634db1
unknown <Wolk007>
2022-08-25 15:43:04 +0300

    задача 4

--- src/Lesson4/extra_task.js
+++ src/Lesson4/extra_task.js
@@ -1,9 +1,7 @@
-let a, b, c;
-a = 50;
-b = a * 5;
-c = a / b;
-c = (a / b) * 10;
-alert((a = 50));
-alert((b = a * 5));
-alert((c = a / b));
-alert((c = (a / b) * 10));
+let a = 50;
+let b = a * 5;
+let c = a / b;
+alert(a);
+alert(b);
+alert(c);
+alert(c * 10);
